2f9202f0191f7ca427ddf868009b0b21f2dda672
Rv054 <83648119+Rv054@users.noreply.github.com>
2023-11-22 17:24:16 +0100

    Delete Code/src directory

--- Code/src/uart_tx_a.vhd
+++
@@ -1,8 +0,0 @@
--- -------------------------------------------------------
--- The files contains UART Transmitter architecture.
--- Author :- Rishi Vaghasiya (35869)
--- Wintersemester 2023/24
--- -------------------------------------------------------
-
-architecture of rv_UART_TX_a of rv_UART_TX_e is 
-end rv_UART_TX_a;
