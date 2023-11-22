40185a446a53f65f3b2bde6bfe674ae6163fdecb
Rv054 <83648119+Rv054@users.noreply.github.com>
2023-11-22 22:45:13 +0100

    Update uart_rx_tb.vhd

--- UART/tb/uart_rx_tb.vhd
+++ UART/tb/uart_rx_tb.vhd
@@ -1,35 +1,23 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 
 entity tb_rv_UART_RX_e is
 end tb_rv_UART_RX_e;
 
 architecture tb_rv_UART_RX_a of tb_rv_UART_RX_e is
-    signal clk : std_logic := '0';
-    signal rst : std_logic := '1'; -- Active low reset
-    signal br_i : std_logic := '0'; -- Baudrate signal
-    signal br2_i : std_logic := '0'; -- Baudrate 2 signal
-    signal rx_i : std_logic := '1'; -- Start with idle state
 
-    signal cry_o : std_logic;
-    signal ascii_o : std_logic_vector(7 downto 0);
-    signal rx_o : std_logic;
+    signal clk_s  : std_logic := '0';
+    signal rst_s  : std_logic := '1'; -- Active low reset
+    signal br_s   : std_logic := '0'; -- Baudrate signal
+    signal br2_s  : std_logic := '0'; -- Baudrate 2 signal
+    signal rx_s   : std_logic := '1'; -- Start with idle state
 
-    -- Clock process
-    process
-    begin
-        wait for 5 ns; -- Initial wait
-        while now < 1000 ns loop
-            clk <= not clk;
-            wait for 5 ns; -- Clock period
-        end loop;
-        wait;
-    end process;
+    signal cry_s    : std_logic;
+    signal ascii_s  : std_logic_vector(7 downto 0);
+    signal rx_s     : std_logic;
 
-    -- DUT instantiation
-    COMPONENT rv_UART_RX
+    COMPONENT rv_UART_RX_e
         port (
             clk_i    : in  std_logic;
             rst_i    : in  std_logic;
@@ -42,48 +30,11 @@ architecture tb_rv_UART_RX_a of tb_rv_UART_RX_e is
         );
     end COMPONENT;
 
-    -- Stimulus process
-    process
     begin
-        wait for 10 ns; -- Initial wait
+        UUT : tb_rv_UART_RX_e port map (clk_s, rst_s, br_s, br2_s, rx_s, cry_s, ascii_s, rx_s);
 
-        -- Reset sequence
-        rst <= '0';
-        wait for 20 ns;
-        rst <= '1';
+        process
+        begin
 
-        -- Test scenario 1: Baudrate 1, sending ASCII 'A'
-        br_i <= '1';
-        br2_i <= '0';
-        rx_i <= '0'; -- Start bit
-        wait for 10 ns;
-        rx_i <= '1'; -- ASCII 'A' (01000001) - 8 bits
-        wait for 50 ns;
-
-        -- Test scenario 2: Baudrate 2, sending ASCII 'B'
-        br_i <= '0';
-        br2_i <= '1';
-        rx_i <= '0'; -- Start bit
-        wait for 10 ns;
-        rx_i <= '1'; -- ASCII 'B' (01000010) - 8 bits
-        wait for 50 ns;
-
-        -- Add more test scenarios as needed
-
-        wait;
-    end process;
-
-    -- Instantiate the DUT
-    UUT: rv_UART_RX
-        port map (
-            clk_i   => clk,
-            rst_i   => rst,
-            br_i    => br_i,
-            br2_i   => br2_i,
-            rx_i    => rx_i,
-            cry_o   => cry_o,
-            ascii_o => ascii_o,
-            rx_o    => rx_o
-        );
 
 end tb_rv_UART_RX_a;
